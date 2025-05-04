daec2007613933fcdb0ebae2704fb266c2dc2c01
FasinFasi <fasingit17@gmail.com>
2025-05-04 13:44:36 +0530

    made some change for deploy

--- app/main.py
+++ app/main.py
@@ -48,4 +48,5 @@ def index():
     return "Flask API is running!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
